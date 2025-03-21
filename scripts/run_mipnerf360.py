e1ea7d9efb154d1f1da6a4e14f4be74f73b89178
ndming <ndminh1101@gmail.com>
2025-03-21 18:11:58 +0100

    Add script to run Mip NeRF 360

--- scripts/run_mipnerf360.py
+++ scripts/run_mipnerf360.py
@@ -1,17 +1,22 @@
 import os
 
-scene_dir = "/home/ndming/datasets/360_v2"
-scenes = ["bicycle", "bonsai", "counter", "flowers", "garden", "kitchen", "room", "stump", "treehill"]
-factors = [2] * len(scenes)
+scene_dir = "/home/zodnguy1/datasets/mipnerf360"
+
+scenes = ["bicycle", "flowers", "garden", "stump", "treehill", "room", "counter", "kitchen", "bonsai"]
+factors = [4, 4, 4, 4, 4, 2, 2, 2, 2]
 
 out_dir = "output/mipnerf360"
 
 for scene, factor in zip(scenes, factors):
-    print(f"======= Processing scene {scene_dir}/{scene} with factor {factor} =======")
-    cmd = f"python train.py -s {scene_dir}/{scene} -m {out_dir}/{scene} -r {factor} --checkpoint_iterations 7000 30000 --eval"
+    print(f"======= Processing scene {scene} with factor {factor} =======")
+
+    cmd = f"python train.py -s {scene_dir}/{scene} -m {out_dir}/{scene} -i images_{factor} --eval --checkpoint_iterations 7000 30000"
     os.system(cmd)
-    cmd = f"python render.py -m {out_dir}/{scene} --skip_train"
+
+    cmd = f"python render.py -m {out_dir}/{scene} --eval --skip_train --iteration 30000"
     os.system(cmd)
+
     cmd = f"python metrics.py -m {out_dir}/{scene}"
     os.system(cmd)
+
     print(f"======= Done with scene {scene_dir}/{scene} =======")
